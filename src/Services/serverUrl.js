ffbff0da933706b3be33e3749b3999ebf8ed4541
sinanktkl <muhammedsinan000001@gmail.com>
2025-09-17 12:45:15 +0530

    first commit

---
+++ src/Services/serverUrl.js
@@ -0,0 +1,5 @@
+// export const serverUrl = "https://backendcr-ncu7.onrender.com"
+// export const serverUrl = "http://localhost:3000"
+export const serverUrl =   import.meta.env.VITE_SERVER_URL || "http://localhost:5000";
+
+       
